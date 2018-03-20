2a58fa540a693b03d45587f9cbf0e6d8981a17ca
Luiz Braga <luizbragaa@gmail.com>
2018-03-20 17:25:24 -0300

    Redux containers

---
+++ Redux/book_list/src/containers/book_list.js
@@ -0,0 +1,43 @@
+/*
+* Instead of use Component, use Container. Container is from React-Redux, which
+* creates the bridge from those two libraries.
+*
+* >> We want the most *parent* component that cares about a
+* particular piece of state to be a Container
+*/
+
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class BookList extends Component {
+
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  // Whatever object is returned will show up as props
+  // inside of BookList
+
+  // When the state changes, the container will change as well
+  return {
+    books: state.books
+  }
+}
+
+// Conection from Redux
+export default connect(mapStateToProps)(BookList);
---
+++ Redux/book_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: Hey'},
+    {title: 'Docker FTW'},
+    {title: 'Python is Life'},
+    {title: 'Dark Knight'}
+  ]
+}
